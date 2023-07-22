3af0679a4e8323aa768aef3ff168eefe48a0b2a1
Igor-andreev1 <man2123@mail.ru>
2023-07-22 20:32:05 +0300

    init

---
+++ src/routing/AppRouter.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {
+    createBrowserRouter, createRoutesFromElements, Route,
+    RouterProvider,
+} from "react-router-dom";
+import Guest from "../pages/Guest/Guest";
+
+
+const router = createBrowserRouter(createRoutesFromElements(
+    <Route>
+        <Route path="/" element={<Guest />}>
+
+        </Route>
+    </Route>
+));
+
+const AppRouter = () => {
+    return (
+        <RouterProvider router={router}/>
+    );
+};
+
+export default AppRouter;
